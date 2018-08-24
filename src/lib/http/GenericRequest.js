fae9acd458d9857c4c93ad21ebade9473bc94897
aetheryx <zain1@live.nl>
2018-08-25 01:59:23 +0200

    fix UA

--- src/lib/http/GenericRequest.js
+++ src/lib/http/GenericRequest.js
@@ -24,7 +24,7 @@ module.exports = class GenericRequest {
       uri,
       query: {},
       headers: {
-        'User-Agent': `SpotifyConnectBot/${version} (https://github.com/aetheryx/SpotifyConnect)`
+        'User-Agent': `SpotifyConnectBot/${version} (https://github.com/aetheryx/spotify-connect)`
       }
     };
   }
