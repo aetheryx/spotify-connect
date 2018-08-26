86aa3b62aa7254e479bac22c849ab7940684b6cc
aetheryx <zain1@live.nl>
2018-08-27 00:10:00 +0200

    descriptions

--- src/commands/device.js
+++ src/commands/device.js
@@ -4,15 +4,14 @@ const { spotifyOAuth } = require('@sc/rest');
 module.exports = class DevicesCommand extends LinkedCommand {
   constructor (main) {
     super(main, {
-      description: 'TODO',
-      usage: 'TODO',
+      description: '__WORK IN PROGRESS__ Lists all of your devices running Spotify.',
       examples: [ '{c}', 'TODO' ],
-      triggers: [ 'device', 'devices' ],
+      triggers: [ 'devices', 'device' ],
       order: 12
     });
   }
 
-  async execute (link, msg, args) {
+  async execute (link) {
     const { devices } = await spotifyOAuth.getDevices(link);
     if (!devices[0]) {
       return 'I was unable to find any devices that are running Spotify on this account.';
