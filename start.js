369680b1e130fa919d2ea5c6d247038398d73a07
Adam <adamxu59@gmail.com>
2018-10-02 21:56:30 -0700

    update start.js
    honestly i just want the t-shirt dude

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
-const SpotifyConnect = require(`${__dirname}/src/`);
+const Tweetcord2 = require(`${__dirname}/src/`);
 
 const config = require(`${__dirname}/config.json`);
 config.BOT_ID = Buffer.from(config.BOT_TOKEN.split('.')[0], 'base64').toString();
@@ -7,4 +7,4 @@ for (const key in config) {
   process.env[key] = config[key];
 }
 
-new SpotifyConnect(config).init();
+new Tweetcord2(config).init();
